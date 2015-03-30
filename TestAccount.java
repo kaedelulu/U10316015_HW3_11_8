1131b05ebe24317651e769854cb09d67c95dae77
kaedelulu <luinghfrhrfuiy@gmail.com>
2015-03-30 19:59:27 +0800

    TestAccount.java
    main class

---
+++ TestAccount.java
@@ -0,0 +1,32 @@
+/**
+ * Name : 呂芝瑩
+ * ID : U10316015
+ * EX : 11.8
+ */
+public class TestAccount {
+	  public static void main (String[] args) {
+		  Account1.setAnnualInterestRate(5.5);
+
+		  Account1 account = new Account1("George", 1122, 1000);	//invoke account
+		  account.deposit(30);
+		  account.deposit(40);
+		  account.deposit(50);
+	    
+		  account.withdraw(5);
+		  account.withdraw(4);
+		  account.withdraw(2);
+		  
+		  System.out.println("Name: " + account.getName());
+		  System.out.println("Annual interest rate: " + Account1.getAnnualInterestRate());
+		  System.out.println("Balance: " + account.getBalance());
+	    
+		  java.util.ArrayList list = account.getTransactions();
+	    
+		  System.out.printf("%-35s%-15s%-15s%-15s\n", "Date", "Type", "Amount", "Balance");
+	    
+		  for (int i = 0; i < list.size(); i++) {
+			  Transaction transaction = (Transaction)(list.get(i));
+			  System.out.printf("%-35s%-15s%-15s%-15s\n", transaction.getDate(), transaction.getType(), transaction.getAmount(), transaction.getBalance());
+		  }
+	  }
+}
